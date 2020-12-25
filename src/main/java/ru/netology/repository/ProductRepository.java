5ad0667e9761e8014c80efcb6a21cdfe5938e646
MVGIC <ex3ct@mail.ru>
2020-12-25 21:59:39 +0300

    fixed method findById

--- src/main/java/ru/netology/repository/ProductRepository.java
+++ src/main/java/ru/netology/repository/ProductRepository.java
@@ -20,34 +20,26 @@ public class ProductRepository {
         return products;
     }
 
-    public void removeById (int id) {
+    public Product findById(int id) {
         for (Product product : products) {
-            if (product.getId() != id) {
-                throw new NotFoundException();
+            if (product.getId() == id) {
+                return product;
             }
         }
+        throw new NotFoundException("Element with id: " + id + " not found");
+
     }
 
-//    public Product findById(int id) {
-//        for (Product product : products) {
-//            if (product.getId() == id) {
-//                return product;
-//            }
-//        }
-//        return null;
-//
-//    }
-
-//    public void removeById(int id) {
-//        int length = products.length - 1;
-//        Product[] tmp = new Product[length];
-//        int index = 0;
-//        for (Product product : products) {
-//            if (product.getId() != id) {
-//                tmp[index] = product;
-//                index++;
-//            }
-//        }
-//        products = tmp;
-//    }
+    public void removeById(int id) {
+        int length = products.length - 1;
+        Product[] tmp = new Product[length];
+        int index = 0;
+        for (Product product : products) {
+            if (product.getId() != id) {
+                tmp[index] = product;
+                index++;
+            }
+        }
+        products = tmp;
+    }
 }
